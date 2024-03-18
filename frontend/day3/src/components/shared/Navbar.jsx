5bd5f284895344886b700401e94c577c9f9923a5
paranthaman-l <727821tucs149@skct.edu.in>
2024-03-18 16:50:14 +0530

    User Components

---
+++ frontend/day3/src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='h-44 w-full bg-white'>
+            dfkslaj
+        </div>
+    )
+}
+
+export default Navbar;
